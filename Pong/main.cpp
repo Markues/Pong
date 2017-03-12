14a1b7c5666c07ac930340295b9098b6ba95fb1c
Jeff Farris <jeff.markues@gmail.com>
2017-03-12 19:11:52 -0400

    Initial Commit

---
+++ Pong/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pong
+//
+//  Created by Jeff Farris on 3/12/17.
+//  Copyright © 2017 Markues. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
